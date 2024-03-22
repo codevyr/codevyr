a40cb587e3e3839f513f26f382d10d45e58d8638
Maksym Planeta <mplaneta@os.inf.tu-dresden.de>
2024-03-22 16:49:22 +0100

    Add simple graph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
 import Image from 'next/image'
-import React from "react";
+import React, { createRef, use } from "react";
 import { useCallback, useEffect, useState } from 'react';
 import { Layout, Model, TabNode, IJsonModel } from 'flexlayout-react';
-import Editor, { Monaco } from '@monaco-editor/react';
+import { Editor, Monaco } from '@monaco-editor/react';
+import monaco, {editor} from 'monaco-editor';
+import {Uri} from 'monaco-editor/esm/vs/editor/editor.api';
+import CytoscapeComponent from 'react-cytoscapejs';
 
 var json: IJsonModel = {
   global: { "tabEnableFloat": true },
@@ -23,7 +26,7 @@ var json: IJsonModel = {
             children: [
               {
                 type: "tab",
-                name: "One",
+                name: "query-editor",
                 component: "button",
               }
             ]
@@ -34,7 +37,7 @@ var json: IJsonModel = {
             children: [
               {
                 type: "tab",
-                name: "Two",
+                name: "graph-viewer",
                 component: "button",
               }
             ]
@@ -58,47 +61,84 @@ var json: IJsonModel = {
 
 const model = Model.fromJson(json);
 
-function EditorComponent() {
-  const [value, setValue] = useState('// some comment');
+interface EditorProps {
+  query: string;
+  setGraph: (graph: any) => void;
+}
+
+function EditorComponent({ query, setGraph }: EditorProps) {
   const askldUrl = 'api'
+
+  const queryGraph = useCallback((ed: monaco.editor.ICodeEditor) => {
+    console.log('submit-query');
+    fetch(`${askldUrl}/query`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      body: ed.getValue()
+    }).then(response => response.json()
+    ).then(data => {
+      // setGraph(data);
+      console.log(data);
+    });
+  }, []);
 
   const handleEditorWillMount = (monaco: Monaco) => {
     monaco.editor.addEditorAction({
       id: 'submit-query',
       label: 'Submit Query',
       keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter],
-      run: function (ed) {
-        console.log('submit-query');
-        fetch(`${askldUrl}/query`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'text/plain'
-          },
-          body: value
-        })
-        .then(response => response.text())
-        .then(text =>{
-          console.log("RESPONSE", text);
-        })
-      }
+      run: queryGraph
     });
   };
 
-  return <Editor height="90vh" defaultLanguage="javascript" defaultValue={value} beforeMount={handleEditorWillMount} />;
+  return <Editor height="90vh" defaultLanguage="javascript" defaultValue={query} beforeMount={handleEditorWillMount} />;
 }
 
-const factory = (node: TabNode) => {
-  const component = node.getComponent();
-  const name = node.getName();
-  if (name === "One") {
-    return EditorComponent();
-  } else if (name === "Two") {
-    return <div>Contents of Tab #2</div>;
-  }
-  return null;
-};
+interface GraphProps {
+  graph: string;
+}
+
+function GraphViewer({graph} : GraphProps) {
+  let cyRef = createRef<CytoscapeComponent>();
+
+  const elements = [
+    { data: { id: 'one', label: 'Node 1' } },
+    { data: { id: 'two', label: 'Node 2' }},
+    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+  ];
+
+  // useEffect(() => {
+  //   if (cyRef) {
+  //     console.log('cyRef', cyRef);
+  //   }
+  // }, [graph]);
+
+  return <CytoscapeComponent elements={elements} style={{ width: '100%', height: '100%' }} cy={(cy: any) => { cyRef = cy }} />;
+}
+
+function GraphCode({graph} : GraphProps) {
+  return <>
+   </>;
+}
 
 export default function Home() {
+  const [query, setQuery] = useState('"create_srq" {}');
+  const [graph, setGraph] = useState({"nodes": [], "edges": []});
+
+  const factory = (node: TabNode) => {
+    const component = node.getComponent();
+    const name = node.getName();
+    if (name === "query-editor") {
+      return <EditorComponent query={query} setGraph={setGraph} />;
+    } else if (name === "graph-viewer") {
+      return <GraphViewer graph={graph} />;
+    } else {
+      return <GraphCode graph={graph}/>;
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Layout model={model} factory={factory} />
