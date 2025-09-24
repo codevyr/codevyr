0a0b35d7184d3fb69190ad388ce3a0f0a515b377
Maksym Planeta <mcsim.planeta@gmail.com>
2025-09-25 00:01:44 +0200

    Add plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <Script
+          defer
+          data-domain="ui.codevyr.com"
+          src="https://plausible.codevyr.com/js/script.js"
+        />
+      </head>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   )
 }
