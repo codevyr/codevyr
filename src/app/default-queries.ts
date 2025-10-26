a6e0fc04014da7fa90c5f762db0a8a688f708709
Maksym Planeta <mcsim.planeta@gmail.com>
2025-10-26 14:20:21 +0100

    Update default query
    - Ignore based on packages
    - Add two projects to the example.

--- src/app/default-queries.ts
+++ src/app/default-queries.ts
@@ -1,21 +1,25 @@
 // Default queries for the application
 
 export const DEFAULT_QUERY = `@preamble
-@ignore("builtin")
-@ignore("runtime")
-@ignore("fmt")
-@ignore("context")
-@ignore("os")
-@ignore("klog")
-@ignore("log")
-@ignore("ioutil")
-@ignore("golang.protobuf");
+@ignore(package="builtin")
+@ignore(package="fmt")
+@ignore(package="context")
+@ignore(package="os")
+@ignore(package="log")
+@ignore(package="runtime")
+@ignore(package="internal")
+@ignore(package="ioutil")
+@ignore(package="golang")
+@ignore(package="k8s.io/klog");
+
+@preamble /* Preamble applies the following verbs globally. */
+@project("kubernetes");
 
 /*
  * Below is a simple query that requests:
  *   1. A function called "main"
- *   2. A function called "run", which is a member of the "run" module
- *   3. All functions called by "run"
+ *   2. A function called "Run", which is a member of the "cli" module
+ *   3. All functions called by "Run"
  *
  * Press Ctrl+Enter to execute the query.
  */
@@ -23,4 +27,11 @@ export const DEFAULT_QUERY = `@preamble
 "main" {
     "cli.Run" {}
 };
+
+@preamble /* Another preamble overwrites the previous preamble. */
+@project("kueue");
+"main" {
+    "NewTokenBucketRateLimiter"
+};
+
 `;
